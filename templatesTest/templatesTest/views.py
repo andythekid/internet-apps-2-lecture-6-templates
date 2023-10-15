ff0b3913c16148e829ebafa8dfc99a2d63098e4d
Andrey Volkov <andrey@voklaf.ru>
2023-10-16 01:30:33 +0300

    доработка шаблона каталога

--- templatesTest/templatesTest/views.py
+++ templatesTest/templatesTest/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 
 menu = {"Главная":"/", "Каталог":"/catalog", "О сайте":"/about"}
 
+
 def main_page(request):
 	title = "Главная"
 	data = {"title": title, "menu": menu}
 	return render(request, "./index.html", context=data)
 
+
 def catalog_page(request):
 	title = "Каталог"
 	data = {"title": title, "menu": menu}
 	return render(request, "./catalog.html", context=data)
+
 
 def about_page(request):
 	title = "О компании"
