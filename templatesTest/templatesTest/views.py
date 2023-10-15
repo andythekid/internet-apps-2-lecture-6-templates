c7f347e8eb22e4bb8d0b46fb0b3227a77b8e9747
Andrey Volkov <andrey@voklaf.ru>
2023-10-16 00:59:53 +0300

    оптимизация views.py

--- templatesTest/templatesTest/views.py
+++ templatesTest/templatesTest/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render
 
+menu = {"Главная":"/", "Каталог":"/catalog", "О сайте":"/about"}
+
 def main_page(request):
-	menu = {"Главная":"/", "Каталог":"/catalog", "О сайте":"/about"}
 	title = "Главная"
 	data = {"title": title, "menu": menu}
 	return render(request, "./index.html", context=data)
 
 def catalog_page(request):
-	menu = {"Главная":"/", "Каталог":"/catalog", "О сайте":"/about"}
 	title = "Каталог"
 	data = {"title": title, "menu": menu}
 	return render(request, "./catalog.html", context=data)
 
 def about_page(request):
-	menu = {"Главная":"/", "Каталог":"/catalog", "О сайте":"/about"}
 	title = "О компании"
 	data = {"title": title, "menu": menu}
 	return render(request, "./about.html", context=data)
